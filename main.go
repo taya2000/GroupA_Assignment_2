cc2d2b7f2453051fae9ed049940b405e22358b84
tejaswicheripally <tejaswicheripally2023@gmail.com>
2024-02-03 17:13:51 -0500

    Add function to calculate factorial of a number

--- main.go
+++ main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Group A")
+
+	fmt.Println("====Starting the function done by Tejaswi which will calculate the factorial of a number====")
+	var num int
+	fmt.Println("Enter a number:")
+	fmt.Scanln(&num)
+
+	fact := factorial(num)
+	fmt.Println("Factorial of", num, "is", fact)
+	fmt.Println("")
+}
+
+// Created by Tejaswi Cheripally - 500229934
+// This function will calculate the factorial of a number
+
+func factorial(n int) int {
+	result := 1
+	for i := 1; i <= n; i++ {
+		result *= i
+	}
+	return result
 }
